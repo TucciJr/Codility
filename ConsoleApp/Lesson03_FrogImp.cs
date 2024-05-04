03fbd828c594179195568483da00663e4f76fb2b
Jose Tucci Jr <tucci.jr@gmail.com>
2024-05-03 21:00:03 -0300

    :zap: Code improvements

--- ConsoleApp/Lesson03_FrogImp.cs
+++ ConsoleApp/Lesson03_FrogImp.cs
@@ -6,7 +6,10 @@ internal class Lesson03_FrogImp
         Console.WriteLine("Lesson03_FrogImp");
         Console.WriteLine("-----------------------");
 
+        Run(1, 7, 3);
         Run(10, 85, 30);
+        Run(10, 1000000000, 1);
+        Run(1, 999111321, 1);
 
         Console.WriteLine("--- * ---");
     }
@@ -20,15 +23,22 @@ internal class Lesson03_FrogImp
 
     public int solution(int X, int Y, int D)
     {
-        var jumps = 0;
+        var jumps = (Y - X) / D;
 
-        while (X < Y)
+        if ((Y - X) % D > 0)
         {
-            X += D;
-
             jumps++;
         }
 
         return jumps;
     }
 }
+/*
+X = 10
+Y = 85
+D = 30
+
+Distance = Y - X        = 75
+Jumps    = Distance / D = 2.5
+
+*/
