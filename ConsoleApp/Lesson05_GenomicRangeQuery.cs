46ceb2e10bdc8b198ff477c67cfa099b8493c9ea
Jose Tucci Jr <tucci.jr@gmail.com>
2024-05-07 01:01:12 -0300

    :art: Finish coding

--- ConsoleApp/Lesson05_GenomicRangeQuery.cs
+++ ConsoleApp/Lesson05_GenomicRangeQuery.cs
@@ -11,7 +11,7 @@ internal class Lesson05_GenomicRangeQuery
         Console.WriteLine("--------------------------");
 
         Run("CAGCCTA", [2, 5, 0, 2], [4, 5, 6, 2]);
-        //Run("A", [0], [0]);
+        Run("A", [0], [0]);
 
         Console.WriteLine("--- * ---");
     }
@@ -19,9 +19,6 @@ internal class Lesson05_GenomicRangeQuery
     private void Run(string S, int[] P, int[] Q)
     {
         var impactFactors = solution(S, P, Q);
-        Console.WriteLine($"Min Impact Factors: [{string.Join(",", impactFactors)}] Sequence: {S} for P: [{string.Join(",", P)}], Q: [{string.Join(",", Q)}]");
-
-        impactFactors = solution3(S, P, Q);
         Console.WriteLine($"Min Impact Factors: [{string.Join(",", impactFactors)}] Sequence: {S} for P: [{string.Join(",", P)}], Q: [{string.Join(",", Q)}]");
     }
 
@@ -90,133 +87,11 @@ internal class Lesson05_GenomicRangeQuery
 
         return result;
     }
-    /*
-    A: [0,1,1,1,1,1,2]
-    C: [1,1,1,2,3,3,3]
-    G: [0,0,1,1,1,1,1]
-    T: [0,0,0,0,0,1,1]
-    */
-
-    public int[] solution3(string S, int[] P, int[] Q)
-    {
-        int[] result = new int[P.Length];
-
-        Dictionary<char, int> impactMap = new Dictionary<char, int>();
-        impactMap.Add('A', 1);
-        impactMap.Add('C', 2);
-        impactMap.Add('G', 3);
-        impactMap.Add('T', 4);
-
-        int[][] prefixSums = new int[4][];
-        for (int i = 0; i < 4; i++)
-        {
-            prefixSums[i] = new int[S.Length + 1];
-
-            //Console.WriteLine($"{i} - prefixSums: [{string.Join(",", prefixSums[i])}]");
-        }
-
-        for (int i = 0; i < S.Length; i++)
-        {
-            int impactIndex = impactMap[S[i]] - 1;
-            for (int j = 0; j < 4; j++)
-            {
-                prefixSums[j][i + 1] = prefixSums[j][i] + (j == impactIndex ? 1 : 0);
-                //Console.WriteLine($"{i} - {j} - impactIndex-1: {impactIndex} - S[{i}]: {S[i]} - prefixSums[j][i]: {prefixSums[j][i]} - prefixSums[{j}][{i + 1}]: [{string.Join(",", prefixSums[j][i + 1])}]");
-            }
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-            Console.WriteLine($"{i} - prefixSums{i}: [{string.Join(",", prefixSums[i])}]");
-        }
-
-        for (int i = 0; i < P.Length; i++)
-        {
-            int start = P[i];
-            int end = Q[i] + 1;
-
-            for (int j = 0; j < 4; j++)
-            {
-                var valueEnd = prefixSums[j][end];
-                var valueStart = prefixSums[j][start];
-                var diff = valueEnd - valueStart;
-
-                if (diff > 0)
-                {
-                    result[i] = j + 1;
-                    break;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    public int[] solution2(string S, int[] P, int[] Q)
-    {
-        var nucleo = new int[S.Length + 1, 4];
-        for (var count = 0; count < S.Length; count++)
-        {
-            if (count > 0)
-            {
-                for (var index = 0; index < 4; index++)
-                {
-                    nucleo[count + 1, index] += nucleo[count, index];
-                }
-            }
-            switch (S[count])
-            {
-                case 'A':
-                    nucleo[count + 1, 0]++;
-                    break;
-                case 'C':
-                    nucleo[count + 1, 1]++;
-                    break;
-                case 'G':
-                    nucleo[count + 1, 2]++;
-                    break;
-                case 'T':
-                    nucleo[count + 1, 3]++;
-                    break;
-            }
-
-            Console.WriteLine($"{count} - S[count]: {S[count]} - nucleo[{count + 1}, 0]: {nucleo[count + 1, 0]} - nucleo[{count + 1}, 1]: {nucleo[count + 1, 1]} - nucleo[{count + 1}, 2]: {nucleo[count + 1, 2]} - nucleo[{count + 1}, 3]: {nucleo[count + 1, 3]}");
-        }
-
-        var result = new int[P.Length];
-        for (var count = 0; count < P.Length; count++)
-        {
-            if (P[count] == Q[count])
-            {
-                switch (S[P[count]])
-                {
-                    case 'A':
-                        result[count] = 1;
-                        break;
-                    case 'C':
-                        result[count] = 2;
-                        break;
-                    case 'G':
-                        result[count] = 3;
-                        break;
-                    case 'T':
-                        result[count] = 4;
-                        break;
-                }
-            }
-            else
-            {
-                for (var index = 0; index < 4; index++)
-                {
-                    if ((nucleo[Q[count] + 1, index] - nucleo[P[count], index]) > 0)
-                    {
-                        result[count] = index + 1;
-                        break;
-                    }
-                }
-            }
-        }
-
-        return result;
-    }
 }
+
+/*
+A: [0,1,1,1,1,1,2]
+C: [1,1,1,2,3,3,3]
+G: [0,0,1,1,1,1,1]
+T: [0,0,0,0,0,1,1]
+*/
